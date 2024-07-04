8d62bb827f19cebb17b64862c2b96a5ff595b2fd
Avihai Zeligman <avihai789@gmail.com>
2024-07-04 13:49:58 +0300

    Connected try again button

--- Paratrooper_game/Assets/Scripts/Presenter/LevelEnd.cs
+++ Paratrooper_game/Assets/Scripts/Presenter/LevelEnd.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
     public void OnClick()
     {
-        Debug.Log("Try again");
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
     }
 }
